c8db73d63fa94e48a67626d6c8c4fa484027753a
DB <3823705+dineshbhagat@users.noreply.github.com>
2018-09-24 18:08:29 +0530

    Update ApplicationConfiguration.java

--- src/main/java/com/example/configuration/ApplicationConfiguration.java
+++ src/main/java/com/example/configuration/ApplicationConfiguration.java
@@ -1,15 +1,23 @@
 package com.example.configuration;
 
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import javax.persistence.EntityManagerFactory;
 
 @SpringBootConfiguration
 public class ApplicationConfiguration {
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
 
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        final ObjectMapper MAPPER = new ObjectMapper();
+        MAPPER.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return MAPPER;
+    }
 
 }
