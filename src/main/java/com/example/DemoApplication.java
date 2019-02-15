7b8dd0f58d341492ee84a12200b121bb62abb177
DB <3823705+dineshbhagat@users.noreply.github.com>
2019-02-15 14:14:29 +0530

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.text.NumberFormat;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
