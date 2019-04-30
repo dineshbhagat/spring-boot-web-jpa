71a22ef7578f3f2d14c119a34bdbb78411a5407e
DB <3823705+dineshbhagat@users.noreply.github.com>
2019-04-30 15:19:09 +0530

    Create MangaResult.java

---
+++ src/main/java/com/example/dto/MangaResult.java
@@ -0,0 +1,14 @@
+package com.example.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MangaResult {
+    private List<Manga> result;
+}
