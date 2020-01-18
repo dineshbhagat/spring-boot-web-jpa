0e87beec0f47802e98e0dd407075d3e4049f5a40
DB <3823705+dineshbhagat@users.noreply.github.com>
2020-01-18 15:54:06 +0530

    Update and rename src/main/java/com/example/notepad/OrgViews to src/main/java/com/example/notepad/jsonviews/OrgViews.java

--- src/main/java/com/example/notepad/jsonviews/OrgViews.java
+++ src/main/java/com/example/notepad/jsonviews/OrgViews.java
@@ -1,4 +1,4 @@
-package com.dkb.jsonviews;
+package com.example.notepad.jsonviews;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MapperFeature;
