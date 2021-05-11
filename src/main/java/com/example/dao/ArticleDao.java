f1b6cbc1ce06fc1a589c6af0e5b828a0970e6d92
DB <3823705+dineshbhagat@users.noreply.github.com>
2021-05-11 18:42:43 +0530

    Update ArticleDao.java

--- src/main/java/com/example/dao/ArticleDao.java
+++ src/main/java/com/example/dao/ArticleDao.java
@@ -5,6 +5,20 @@ import com.example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+Note: If you prefer to be selective about the methods being exposed, copy the methods you want to expose from CrudRepository into your domain repository
+Doing so lets you define your own abstractions on top of the provided Spring Data Repositories functionality. 
+
+public interface myRestrictedArticleDao extends JpaRepository<Article, Long> {
+   Optional<T> findById(ID id);
+}
+
+@Repository
+public interface ArticleDao extends myRestrictedArticleDao<Article, Long> {
+}
+
+Ref: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#preface
+*/
 @Repository
 public interface ArticleDao extends JpaRepository<Article, Long> {
 }
